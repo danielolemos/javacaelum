94f7236a35d670d52cbbb299117020508ad90024
daniel <daniel@daniel-NE570>
2015-01-21 20:37:44 -0200

    Terceito Commit de Teste

--- Caelum_Algoritmos/src/br/caelum/estruturaalunos/TesteCommit.java
+++ Caelum_Algoritmos/src/br/caelum/estruturaalunos/TesteCommit.java
@@ -3,7 +3,7 @@ package br.caelum.estruturaalunos;
 public class TesteCommit {
 
 	public static void main(String[] args) {
-		System.out.println("Teste Commit2 Do Github ao Vivo");
+		System.out.println("Teste Commit2 Do Github ao Vivo Duas Vezes na Lua");
 	}
 
 }
