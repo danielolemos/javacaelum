062b7580ca82fc696abf567a5063920f0c8b415b
Daniel <Daniel@Daniel-PC>
2015-01-22 17:27:26 -0200

    Commitando pela quinta vez

--- Caelum_Algoritmos/src/br/caelum/estruturaalunos/TesteCommit.java
+++ Caelum_Algoritmos/src/br/caelum/estruturaalunos/TesteCommit.java
@@ -4,6 +4,7 @@ public class TesteCommit {
 
 	public static void main(String[] args) {
 		System.out.println("Teste Commit2 Do Github ao Vivo Duas Vezes na Lua Na Parceria dos Maluco");
+		System.out.println("As cavalas 123 nao sabem outra vez");
 	}
 
 }
