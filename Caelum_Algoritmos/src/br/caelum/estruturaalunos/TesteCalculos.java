28807142d8f245c42993553c3c63f37e1662044c
daniel <daniel@daniel-K43E>
2015-01-24 23:20:17 -0200

    teste

---
+++ Caelum_Algoritmos/src/br/caelum/estruturaalunos/TesteCalculos.java
@@ -0,0 +1,24 @@
+package br.caelum.estruturaalunos;
+
+public class TesteCalculos {
+
+	public static void main(String[] args) {
+		
+		int total = 8;
+		
+		for (int i = total - 1; i >= 0; i-=1) {
+			System.out.println(i + 1 + " <- " + i);
+		}
+		
+		
+		
+		/*
+		int j = i - 1;System.out.println(j);
+		if (j >= 3) {
+			System.out.println(j-=1);
+		}
+		*/
+
+	}
+
+}
